7ccb2297c2827fe82397f7d125ecbdff6d0aa0e8
Riyan <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2022-11-23 10:24:01 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::middleware('isGuest')->group(function () {
+    Route::get('/', [TodoController::class, 'login'])->name('login');
+    Route::get("register", [TodoController::class, 'register']);
+    Route::post('/register', [TodoController::class, 'inputRegister'])->name('register.post');
+    Route::post('/index', [TodoController::class, 'auth'])->name('loginauth');
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+});
+
+Route::get('/logout', [TodoController::class, 'logout'])->name('logout');
+
+//todo
+Route::middleware('isLogin')->prefix('/todo')->name('todo.')->group(function () {
+    Route::get('/', [TodoController::class, 'index'])->name('index');
+    Route::get('/complated', [TodoController::class, 'complated'])->name('complated');
+    Route::get('/create', [TodoController::class, 'create'])->name('create');
+    Route::post('/store', [TodoController::class, 'store'])->name('store');
+});
